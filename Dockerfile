aacbca6be219a5cf8e66dbab3c9a7b3a91e6fd82
TheLastBilly <jtmonegro@gmail.com>
2020-10-26 21:39:04 -0400

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM ubuntu:20.04
+
+ENV DEBIAN_FRONTEND noninteractive
+RUN apt update && apt install -y \
+        libfreetype6-dev \
+        libjpeg62-dev \
+        libpng-dev \
+        git nginx gettext openssl php-fpm=2:7.4+75 php7.4-pgsql npm composer php7.4-curl php7.4-dom php7.4-gd
+
+RUN mkdir /easyappointments
+WORKDIR /easyappointments
+
+COPY ./gen_certs.sh /gen_certs.sh
+COPY ./config.php.template /config.php.template
+COPY ./nginx.conf /etc/nginx/conf.d/nginx.conf
+COPY ./entrypoint.sh /entrypoint.sh
+
+RUN apt install php-mysqlnd
+
+CMD /entrypoint.sh
---
+++ entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+if [ ! -f /easyappointments/config.php ]; then
+    rm -rf /easyappointments/*
+
+    cd /
+    git clone --depth=1 https://github.com/alextselegidis/easyappointments easyappointments
+
+    cd easyappointments
+    npm install
+    composer install
+fi
+
+chown www-data:www-data -R /easyappointments
+envsubst < "/config.php.template" > "/easyappointments/config.php"
+
+if [ -f "/certs/recreate" ]; then
+    cd /
+    /gen_certs.sh
+    cat cert.crt > /certs/cert.crt
+    cat cert.key > /certs/cert.key
+
+    rm /certs/recreate
+fi
+
+/usr/sbin/php-fpm7.4 &
+nginx -g 'daemon off;'
